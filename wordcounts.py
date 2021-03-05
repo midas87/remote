e840d9fbe53c6180bb8d28596b1a5ae28bb1c943
midas <makaveliddon87@gmail.com>
2021-03-05 00:12:23 -0500

    find the most occur/common character and print it out

--- wordcounts.py
+++ wordcounts.py
@@ -4,13 +4,15 @@ fileNote = open('dicBook.txt')
 fileRead = fileNote.readlines()
 
 myDict = {}
+count = 0
 
 for read in fileRead:
     noWhitespace = read.strip()
     nameList = noWhitespace.split()
-    #print(nameList)
+
     for name in nameList:
-        #if name not in myDict:
         myDict[name] =  myDict.get(name, 0) + 1
 
+maxNum = max(myDict, key=myDict.get)
 print(myDict)
+print('The most common word is:',maxNum)
