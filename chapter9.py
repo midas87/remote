694ac088b84f7d0ee8f101a03c6e8f222aca2ce5
midas <makaveliddon87@gmail.com>
2021-03-07 19:46:08 -0500

    makes the code cleaning

--- chapter9.py
+++ chapter9.py
@@ -1,12 +1,5 @@
 # calling the file and reading from it
 
-# fileName =
-'''
-with open('intro.txt') as file:
-    fileName = file.read()
-    print(fileName)
-
-'''
 
 dicWord = {}
 
@@ -14,13 +7,12 @@ inputFile = input('Enter the file name: ')
 try:
     with open(inputFile) as newFile:
         fileName = newFile.readlines()
-        print(fileName)
+        #print(fileName)
 except FileNotFoundError:
     print(inputFile, 'file not found')
 for run in fileName:
     fileSplit = run.strip().split()
     for word in fileSplit:
         dicWord[word] = dicWord.get(word, 0) + 1
-    print(fileSplit)
-    print(dicWord)
-    print(max(dicWord))
+print(dicWord)
+print(max(dicWord))
