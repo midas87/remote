bf249569aac759eadce0a63eeff67ab704656ea2
midas <makaveliddon87@gmail.com>
2021-03-09 03:35:23 -0500

    write a code to retrieve/read data file on webserver

---
+++ urlData.py
@@ -0,0 +1,26 @@
+# Reading data from Webpage
+
+import urllib.request,urllib.parse, urllib.error
+
+fHand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+count = {}
+
+for line in fHand:
+    #print(line.decode().strip())
+    splFile = line.decode().strip().split()
+    for check in splFile:
+        count[check] = count.get(check, 0) + 1
+
+getMax = max(count, key=count.get)
+print(count)
+
+#print(getMax)
+
+num = 0
+
+for k, v in count.items():
+    if v > num:
+        num = v
+print('The most common character is:',getMax,'and the max value is',num)
+
