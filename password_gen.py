74d1f3cbd8f36d68b5b562716f2ef26462ca34fd
midas <makaveliddon87@gmail.com>
2021-03-12 00:37:49 -0500

    added random integer to the code and make it mode professional

--- password_gen.py
+++ password_gen.py
@@ -1,29 +1,41 @@
 # My Password generator
 
+### Start the code on 8th of March 2021 ###
+### Finish on 12th of March 2021 12:00 AM ###
+
+#print('***')
+#print('--***')
+#print('----***''\n')
+
+print('Welcome to my Password Generator''\n')
+
+
 import random
 
 password = []
-myString = ''
+listString = ''
 
 myString = 'abcdefghijklmnopqrstuvwxyz'
 specialChart = "!@#$%^&*?~"
 capString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-maxstringLen = 8
-stringLen = 4
+maxstringLen = 10
+
 
 for word in myString:
     if word not in password and len(password) < maxstringLen:
         chk = random.choice(myString)
         password.append(chk)
-        #print(len(password))
-    #for n in specialChart:
     if specialChart not in password and len(password) < maxstringLen:
         sChart = random.choice(specialChart)
         password.append(sChart)
     if capString not in password and len(password) < maxstringLen:
         sCap = random.choice(capString)
         password.append(sCap)
-    #if password is not ='':
+    if password != '' and len(password) < maxstringLen:
+        intS = random.randint(1,9)
+        password.append(intS)
 
-print(password)
+print(password,'\n')
+newListString = ''.join([str(item) for item in password])
+print('Password:',newListString)
