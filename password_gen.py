b7de98c119bbdf7d19ba601c315258044bde0375
midas <makaveliddon87@gmail.com>
2021-03-11 23:59:31 -0500

    modify the code to add more character to the password list

--- password_gen.py
+++ password_gen.py
@@ -3,17 +3,27 @@
 import random
 
 password = []
+myString = ''
 
 myString = 'abcdefghijklmnopqrstuvwxyz'
-specialChart = "!@#$%^&*()[]{}?~"
+specialChart = "!@#$%^&*?~"
 capString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 maxstringLen = 8
 stringLen = 4
 
 for word in myString:
-    if len(word) < stringLen:
+    if word not in password and len(password) < maxstringLen:
         chk = random.choice(myString)
-
         password.append(chk)
+        #print(len(password))
+    #for n in specialChart:
+    if specialChart not in password and len(password) < maxstringLen:
+        sChart = random.choice(specialChart)
+        password.append(sChart)
+    if capString not in password and len(password) < maxstringLen:
+        sCap = random.choice(capString)
+        password.append(sCap)
+    #if password is not ='':
+
 print(password)
