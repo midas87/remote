681f0f53f0eff5ee437a200f5bca08111b97f069
midas <makaveliddon87@gmail.com>
2021-03-13 21:46:56 -0500

    add a variable option for password lenght

--- password_gen.py
+++ password_gen.py
@@ -4,32 +4,35 @@
 ### Finish on 12th of March 2021 12:00 AM ###
 
 
-print('Welcome to my Password Generator''\n')
+print('Welcome to Password Generator''\n')
 
+print('Password length: ', end='')
 
 import random
 
 password = []
+
+maxstringLen = int(input())
 
 
 myString = 'abcdefghijklmnopqrstuvwxyz'
 specialChart = "!@#$%^&*?~"
 capString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-maxstringLen = 10
 
 
 for word in myString:
-    if word not in password and len(password) < maxstringLen:
+
+    if word not in password and len(password) < int(maxstringLen):
         chk = random.choice(myString)
         password.append(chk)
-    if specialChart not in password and len(password) < maxstringLen:
+    if specialChart not in password and len(password) < int(maxstringLen):
         sChart = random.choice(specialChart)
         password.append(sChart)
-    if capString not in password and len(password) < maxstringLen:
+    if capString not in password and len(password) < int(maxstringLen):
         sCap = random.choice(capString)
         password.append(sCap)
-    if password != '' and len(password) < maxstringLen:
+    if password != '' and len(password) < int(maxstringLen):
         intS = random.randint(1,9)
         password.append(intS)
 
