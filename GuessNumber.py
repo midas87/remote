0fcbac40e8ef047cf357e48e67d7f6ccf268aec9
midas <makaveliddon87@gmail.com>
2021-03-17 01:27:01 -0400

    add try and except

--- GuessNumber.py
+++ GuessNumber.py
@@ -7,28 +7,41 @@ numb = random.randint(1,20)
 
 print('You have five guesses')
 
-#print('Guess the number: ', end='')
-
-guess = int(input('Guess the number: '))
 count = 1
 maxCount = 5
 
+#guess = int(input('Guess the number: '))
+
+#print('Guess the number: ', end='')
+try:
+    guess = int(input('Guess the number: '))
+except ValueError:
+    print('Please enter number only')
+
 while True:
+        try:
+            guess = int(input('Guess the number: '))
 
-    if count == maxCount:
-        print('you have max your number of guesses, the guess number is:',numb)
-        break
-    if numb > guess:
-        print('You guess is low, Please try again')
-        guess = int(input('Guess the number: '))
-        count = count + 1
 
-    if numb < guess:
-        print('You guess is high, please choose lower number')
-        guess = int(input('Guess the number: '))
-        count = count + 1
+            if count == maxCount:
+                print('you have max your number of guesses, the guess number is:',numb)
+                break
+            if numb > guess:
+                print('You guess is low, Please try again')
+                guess = int(input('Guess the number: '))
+                count = count + 1
 
-    if numb == guess:
-        print('You got the guess right, the correct number is:',numb)
-        break
+            if numb < guess:
+                print('You guess is high, please choose lower number')
+                guess = int(input('Guess the number: '))
+                count = count + 1
 
+            if numb == guess:
+                print('You got the guess right, the correct number is:',numb)
+                break
+        except ValueError:
+            print('Number Only')
+
+
+#NameError
+#ValueError
