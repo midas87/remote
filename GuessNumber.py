85741fe9948ef0bf1f1501ca606846040be8e5fe
midas <makaveliddon87@gmail.com>
2021-03-17 02:56:43 -0400

    fix the bugs for non integers character and added counting of numbers tries left

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,45 +1,41 @@
-# Number Guess
-
 import random
 
+print('Try your luck, Guessing Game')
+
 numb = random.randint(1,20)
-#print(numb)
 
 print('You have five guesses')
 
-count = 1
+count = 0
 maxCount = 5
 
-
-try:
-    guess = int(input('Guess the number: '))
-except ValueError:
-    print('Please enter number only')
+#guess = int(input('Guess the number: '))
 
 while True:
         try:
             guess = int(input('Guess the number: '))
 
-
             if count == maxCount:
                 print('you have max your number of guesses, the guess number is:',numb)
                 break
+
             if numb > guess:
                 print('You guess is low, Please try again')
-                guess = int(input('Guess the number: '))
+                #guess = int(input('Guess the number: '))
                 count = count + 1
+                countRem = maxCount - count
+                print('You have',countRem,'guesses left')
 
             if numb < guess:
                 print('You guess is high, please choose lower number')
-                guess = int(input('Guess the number: '))
+                #guess = int(input('Guess the number: '))
                 count = count + 1
+                countRem = maxCount - count
+                print('You have', countRem, 'guesses left')
 
             if numb == guess:
                 print('You got the guess right, the correct number is:',numb)
                 break
         except ValueError:
-            print('Number Only')
+            print('Numbers Only')
 
-
-#NameError
-#ValueError
