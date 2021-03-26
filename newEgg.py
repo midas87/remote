540323ad4124d0177734f7bf8754d0a83ef0227d
midas <makaveliddon87@gmail.com>
2021-03-26 02:20:49 -0400

    added more scrapping option to get devices

--- newEgg.py
+++ newEgg.py
@@ -1,18 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
 
-# pageUrl = requests.get('https://www.newegg.com/p/pl?d=nas').text
+pageUrl = requests.get('https://www.newegg.com/p/pl?d=nas').text
 
-bb = requests.get('https://www.bestbuy.ca/en-ca/collection/headphones-on-sale/23058?icmp=pa_categorylanding_shopbycategory_audio_onsale').text
+# bb = requests.get('https://www.bestbuy.ca/en-ca/collection/headphones-on-sale/23058?icmp=pa_categorylanding_shopbycategory_audio_onsale').text
 
-sp = BeautifulSoup(bb, 'lxml')
+sp = BeautifulSoup(pageUrl, 'lxml')
 
-print(sp.prettify())
+# print(sp.prettify())
 
-'''
-containers = sp.find_all('div', class_='productItemRow_hyNOs row_1mOdd')
+
+containers = sp.find_all('div', class_='item-container')
 
 container = containers[0]
 
-print(container)
-'''
+n = container.div.div.a.img['title']
+
+sales = containers = sp.find_all('span', {'class': 'price-save-endtime'})
+
+salesEnd = sales[0].text
+
+shipping = containers = sp.find_all('li', {'class': 'price-ship'})
+
+freeShip = shipping[0].text
+
+link = containers = sp.find_all('a', class_='item-title')
+
+productName = link[0].text
+
+site = container.a['href']
+
+# print(link)
+
+print(freeShip)
+
+print(n)
+
+# print(sales)
+
+print(salesEnd)
+
+print(productName)
+
+print(site)
