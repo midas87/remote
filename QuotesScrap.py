95f9d31dae29b00cc65a450e903c7b6c1a055be7
midas87 <makaveliddon87@gmail.com>
2021-04-10 20:19:29 -0400

    added tag element to the code

--- QuotesScrap.py
+++ QuotesScrap.py
@@ -6,6 +6,7 @@ html_request = requests.get('https://quotes.toscrape.com/').text
 soup = bs(html_request, 'lxml')
 
 pageScrap = soup.find_all('div', {'class': 'quote'})
+tag = soup.find_all('div', {'class': 'tags'})
 
 fileQuote = open('Quotes.text', 'w')
 
@@ -13,8 +14,11 @@ for readLine in pageScrap:
     quote = readLine.find('span', class_='text').text
 
     author = readLine.find('small', {'class': 'author'}).text
-
     fileQuote.write(quote + '\n')
     fileQuote.write(author + '\n')
+
+    for nameTag in tag:
+        newTag = nameTag.find('a', {'class': 'tag'}).text
+        fileQuote.write(newTag + '\n')
 
 fileQuote.close()
