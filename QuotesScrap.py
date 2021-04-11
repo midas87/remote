069b91441da8076c45f8319ed3b40df498d3d046
midas87 <makaveliddon87@gmail.com>
2021-04-10 20:01:13 -0400

    add the tags element to the code

--- QuotesScrap.py
+++ QuotesScrap.py
@@ -7,6 +7,7 @@ html_request = requests.get('https://quotes.toscrape.com/').text
 soup = bs(html_request, 'lxml')
 
 pageScrap = soup.find_all('div', {'class': 'quote'})
+tag = soup.find_all('div', {'class': 'tags'})
 
 fileQuote = open('Quotes.text', 'w')
 
@@ -14,8 +15,11 @@ for readLine in pageScrap:
     quote = readLine.find('span', class_='text').text
 
     author = readLine.find('small', {'class': 'author'}).text
-
     fileQuote.write(quote + '\n')
     fileQuote.write(author + '\n')
+
+    for nameTag in tag:
+        newTag = nameTag.find('a', {'class': 'tag'}).text
+        fileQuote.write(newTag + '\n')
 
 fileQuote.close()
