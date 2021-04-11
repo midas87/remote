5014759f4644ccea24a3751a2b3829c1b5e79eb8
midas87 <makaveliddon87@gmail.com>
2021-04-10 23:01:39 -0400

    added loop to go through all the pages

--- ScrapExercise.py
+++ ScrapExercise.py
@@ -15,4 +15,32 @@ for name in items:
 
     print(f'{counter}, ItemName: {clothType}, Amount: {price}')
     counter = counter + 1
-# print(items)
+
+pages = soup.find('ul', class_='pagination')
+urls = []
+
+links = pages.find_all('a', class_='page-link')
+
+for link in links:
+    pageNum = int(link.text) if link.text.isdigit() else None
+    if pageNum is not None:
+        x = link.get('href')
+        urls.append(x)
+#print(urls)
+
+for i in urls:
+    newUrl = urlPage + i
+    #print(newUrl)
+    #response = requests.get(newUrl)
+    soup = bs(urlPage, 'lxml')
+
+    items = soup.find_all('div', {'class': 'col-lg-4 col-md-6 mb-4'})
+
+    for name in items:
+        clothType = name.find('h4', class_='card-title').a.text
+        price = name.find('h5').text
+
+        print(f'{counter}, ItemName: {clothType}, Amount: {price}')
+        counter = counter + 1
+
+# print(links)
