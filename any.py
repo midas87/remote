6ee043c8575a03e9ff3c745ce6a766235343049d
midas87 <makaveliddon87@gmail.com>
2021-05-23 02:21:48 -0400

    code modify

--- any.py
+++ any.py
@@ -18,7 +18,7 @@ newFile.close()
 failFile.close()
 
 """
-"""
+
 print('Welcome To The Movie Theatre...Where Entertainment is your friend!')
 
 child_ticket = 3
@@ -36,7 +36,7 @@ while True:
         print('Your ticket price is $15')
         break
 
-"""
+
 # Writing an Endless loop for Test.
 
 # while True:
