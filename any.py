9ce90aa01723c58fbc52ed86a4d980d68965ed43
midas87 <makaveliddon87@gmail.com>
2021-05-23 02:20:38 -0400

    modify the movie code and wrote and endless loop

--- any.py
+++ any.py
@@ -18,23 +18,26 @@ newFile.close()
 failFile.close()
 
 """
-
-
+"""
 print('Welcome To The Movie Theatre...Where Entertainment is your friend!')
 
 child_ticket = 3
 teenager_ticket = 12
-# adult_ticket = 12
 
 while True:
     age = int(input('Please enter your age: '))
     if age <= child_ticket:
         print('You get free movie')
         break
-    if age > child_ticket <= teenager_ticket:
+    if child_ticket < age <= teenager_ticket:
         print('Your ticket price is $10')
         break
     else:
         print('Your ticket price is $15')
+        break
 
+"""
+# Writing an Endless loop for Test.
 
+# while True:
+    # print('This is an endless loop running!!!')
