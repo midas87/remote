32ac7538b08f4700f6482d03471f9e6630bd9146
midas87 <makaveliddon87@gmail.com>
2021-05-24 16:22:34 -0400

    added Mountain Poll code

--- any.py
+++ any.py
@@ -82,7 +82,7 @@ for confirmed_user in confirmed_users:
 pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
 
 # print(pets)
-
+'''
 
 while pets:
     pets.remove('cat')
@@ -97,3 +97,30 @@ while 'cat' in pets:
     pets.remove('cat')
 
 print(pets)
+
+'''
+
+# Mountain Poll Code
+
+responses = {}
+
+# Set a flag to indicate that polling is active.
+
+polling_active = True
+
+while polling_active:
+    name = input('What is your name? ')
+    response = input('Which mountain would you like to climb someday? ')
+
+    # store the response in a dictionary
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input('Would you like to let another person respond? (yes/ no) ')
+    if repeat == 'no':
+        polling_active = False
+
+    # Polling is complete, Show the results.
+print("\n--- Poll Result ---")
+for name, response in responses.items():
+    print(f'{name} would like to climb {response}.')
